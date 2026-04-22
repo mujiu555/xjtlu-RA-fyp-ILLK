b7e4d4ccc799436b73ea467b0c6b80235391a257
mujiu555@GitHub <1310769510@qq.com>
2026-04-22 17:18:12 +0800

    feat:

---
+++ src/main.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+
+def find_files_in_directory(directory):
+    """Return a list of file paths in the given directory (non-recursive)."""
+    return [
+        os.path.join(directory, f)
+        for f in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, f))
+    ]
+
+
+def translate_from(file_path):
+    """Read and parse JSON data from the specified file."""
+    try:
+        with open(file_path, "r") as file:
+            sample = json.load(file)
+            idx = sample["idx"]
+            file_name = sample["file_name"]
+            cwe = sample["cwe"]
+            code = sample["code"]["source"]
+            to = {"idx": idx, "file_name": file_name, "cwe": cwe, "code": code}
+            return to
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file {file_path} does not exist.")
+    except json.JSONDecodeError:
+        raise ValueError(f"The file {file_path} does not contain valid JSON.")
+
+
+def main():
+    i = []
+    for f in find_files_in_directory("/root/workspace/xjtlu-RA-fyp-ILLK/test_graphson"):
+        i += translate_from(f)
+    f = open("/root/workspace/workspace/translated.json", "w")
+    f.write(json.dumps(i))
+
+
+if __name__ == "__main__":
+    main()
